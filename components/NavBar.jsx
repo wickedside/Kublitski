d130e695ce18a6557138e4631d14338ae2b3004a
wickedside <78486904+wickedside@users.noreply.github.com>
2021-12-05 21:58:52 +0300

    Revert "Полностью переделал NavBar"

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,55 +1,18 @@
-import React, { useContext, useEffect, useState } from "react";
-import { Link } from "react-router-dom"
-import AuthContext from "../context"
-import {ThemeProvider} from "styled-components";
-import { GlobalStyles } from "../components/globalStyles";
-import { lightTheme, darkTheme } from "../components/Themes";
+import React from 'react';
 
 const NavBar = () => {
-  const {isAuth, setIsAuth} = useContext(AuthContext);
-
-  const [theme, setTheme] = useState('light');
-  const themeToggler = () => {
-    theme === 'light' ? setTheme('dark') : setTheme('light')}
-
-  const logout = () =>{
-    localStorage.clear();
-    setIsAuth(false)
-  }
   return (
-    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
-
-<>
-
-<GlobalStyles/>
-
     <nav>
-    <div className="nav-wrapper">
-      <a href="/" className="brand-logo">Logo</a>
-
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><Link to='/home'>Home</Link></li>
-        <li><Link to='/users'>Users</Link></li>
-        <li><Link to='/posts'>Posts</Link></li>
-        <li><Link to='/devices'>Devices</Link></li>
-        <a className="waves-effect waves-light btn" onClick={() => setIsAuth(false)}>
-          <i className="material-icons right">exit_to_app</i>
-          Log out
-        </a>
-      </ul>
-    </div>
-    <div class="switch">
-       <label>
-         Dark
-         <input type="checkbox"></input>
-         <span class="lever" onClick={themeToggler}></span>
-         Light
-       </label>
-     </div>
-  </nav>
-
-  </>
-  </ThemeProvider>
+        <div class="nav-wrapper">
+            <a href="#" class="brand-logo">Logo</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="#">Sass</a></li>
+            <li><a href="#">Components</a></li>
+            <li><a href="#">JavaScript</a></li>
+            </ul>
+        </div>
+    </nav>
   );
 }
+
 export default NavBar;
